35b9e474755c2456da2f23118790b9c8d66f8bfe
tom <miya4932@gmail.com>
2019-07-20 19:12:11 +0900

    作成したメッセージをメッセージ画面に追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,35 @@
-$(document).on('turbolinks:load', function() {
-
-  function buildHTML(message) {
-    var insertImage = '';
-    if (message.image.url) {
-      insertImage = `<img src="${message.image.url}">`;
-    }
-    var html = `
-      <div class="chat" data-message-id="${message.id}">
-        <p class"chat__user">${message.name}</p>
-        <p class"chat__date">${message.date}</p>
-        <p class"chat__content">${message.content}</p>
-        ${insertImage}
-      </div>`;
-    return html  
+$(document).on('turbolinks:load', function(){
+  function bulidHTML(message) {
+    //var insertImage = '';
+    //if (message.image) {
+      //insertImage = `<img src="${message.image}">`;
+    //}
+    var html =
+      `<div class="chat" data-id="${message.id}">
+        <p class="chat__user">${message.user_name}</p>
+        <p class="chat__date">${message.date}</p>
+        <p class="chat__content">${message.content}</p>
+      </div>`
+    return html;
   }
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var message = new FormData(this);
-    var url = $(this).attr('action')
+    var message = new FormData($(this).get(0));
+    var url = window.location.pathname;
     $.ajax({
       url: url,
-      type: "POST",
+      type: 'POST',
       data: message,
       dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data) {
-      var chat = buildHTML(data);
-      $('chat-wrapper').prepend(chat);
+      console.log(data)
+      var html = bulidHTML(data);
+      $('.chat-wrapper').append(html);
       $('.js-form__text-field').val('');
-    })
-    .fail(function(data) {
-      alert('メッセージを入力してください');
-    })
-    .always(function(data) {
-      alert('テストです');
     })
   })
 });
