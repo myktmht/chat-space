4a2ec011aea8eb5f37c3afb182638831dcdab17a
tom <miya4932@gmail.com>
2019-07-20 13:58:08 +0900

    メッセージの自動追加とスクロールができるように（送信忘れ）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,42 @@
 $(document).on('turbolinks:load', function() {
-  $('#new_form').on('submit', function(e) {
+
+  function buildHTML(message) {
+    var insertImage = '';
+    if (message.image.url) {
+      insertImage = `<img src="${message.image.url}">`;
+    }
+    var html = `
+      <div class="chat" data-message-id="${message.id}">
+        <p class"chat__user">${message.name}</p>
+        <p class"chat__date">${message.date}</p>
+        <p class"chat__content">${message.content}</p>
+        ${insertImage}
+      </div>`;
+    return html  
+  }
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var message = new FormData(this);
+    var url = $(this).attr('action')
     $.ajax({
-        type: 'POST',
-        url: '/messages',
-        data: FormData,
-        processData: false,
-        contenttype: false,
-        dataType: 'json'
+      url: url,
+      type: "POST",
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var chat = buildHTML(data);
+      $('chat-wrapper').prepend(chat);
+      $('.js-form__text-field').val('');
+    })
+    .fail(function(data) {
+      alert('メッセージを入力してください');
+    })
+    .always(function(data) {
+      alert('テストです');
     })
   })
 });
